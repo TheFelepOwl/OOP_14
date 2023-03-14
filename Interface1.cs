91c955db29c036df99691b7fedf21387ee3da7f3
TheFelepOwl <denisgavr04@gmail.com>
2023-03-15 00:42:57 +0100

    Create Interface1.cs

---
+++ Interface1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_14
+{
+    public interface IShape
+    {
+        double GetVolume();
+    }
+}
