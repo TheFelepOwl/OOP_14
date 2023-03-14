6ef390fe8b76024b5c04c62982db33417d94f926
TheFelepOwl <denisgavr04@gmail.com>
2023-03-15 00:43:00 +0100

    Create Class1.cs

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_14
+{
+    public class Bullet : IShape
+    {
+        private double radius;
+
+        public Bullet()
+        {
+            this.radius = 0;
+        }
+
+        public Bullet(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        public double GetVolume()
+        {
+            return (4.0 / 3.0) * Math.PI * Math.Pow(radius, 3);
+        }
+
+        public void print_val()
+        {
+            Console.WriteLine($"Volume: {GetVolume():F2}");
+        }
+    }
+}
